2d25d38d43649585a28a34a56f8bcc712a474708
jesravnbol <jra@kapacity.dk>
2021-05-15 12:57:42 +0200

    Black and flake

--- src/config.py
+++ src/config.py
@@ -12,17 +12,17 @@ def convert_db_uri(uri):
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = "my_precious"
 
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 
 class ProductionConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = convert_db_uri(os.environ.get('DATABASE_URL'))
+    SQLALCHEMY_DATABASE_URI = convert_db_uri(os.environ.get("DATABASE_URL"))
